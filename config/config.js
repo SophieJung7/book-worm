a74797a96002c478ba2b7f063e24dbe4906e5b15
Sophie Jung <sophiecheong7@gmail.com>
2020-09-23 14:17:33 +0800

    git remove cache

--- config/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBE3lmsM78VgNtf_39TWB-AGxkVDuiKPng',
-  authDomain: 'book-worm-17c70.firebaseapp.com',
-  databaseURL: 'https://book-worm-17c70.firebaseio.com',
-  projectId: 'book-worm-17c70',
-  storageBucket: 'book-worm-17c70.appspot.com',
-  messagingSenderId: '1091669608851',
-  appId: '1:1091669608851:web:e827545eb148ec819a0066',
-};
